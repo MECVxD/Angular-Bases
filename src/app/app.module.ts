f7e1d8248f19d78cfce99546ffc7a035173d3d2c
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2022-04-28 01:25:30 -0500

    Saved changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
-
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+//Crear un modulo llamado ContadorModule
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, HeroeComponent, ListadoComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
